cd9bac09f125aa2b08c352e3bb99e9eaba342c8f
premasankaran <116370502+premasankaran@users.noreply.github.com>
2022-10-27 12:25:46 -0700

    Add files via upload

---
+++ compiled.java
@@ -0,0 +1,12 @@
+public class OddEven
+{
+    public static void main(final String[] array) {
+        final int n = 21;
+        if (n % 2 == 0) {
+            System.out.println(n + "is Even");
+        }
+        else {
+            System.out.println(n + "is odd ");
+        }
+    }
+}
