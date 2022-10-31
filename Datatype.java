4d040e9ac823d323eaed41efdd977011d81f03ca
premasankaran <premsiva0304@gmail.com>
2022-10-31 00:30:34 -0700

    file create

---
+++ Datatype.java
@@ -0,0 +1,21 @@
+public class data
+{
+    public static void main(final String[] array) {
+        final boolean x = true;
+        final int x2 = 10;
+        final int x3 = 1000;
+        final char x4 = 'A';
+        final long x5 = 1000000L;
+        final int x6 = 25;
+        final float x7 = 29.3f;
+        final double x8 = 23.84;
+        System.out.println(x);
+        System.out.println(x2);
+        System.out.println(x3);
+        System.out.println(x4);
+        System.out.println(x5);
+        System.out.println(x6);
+        System.out.println(x7);
+        System.out.println(x8);
+    }
+}
